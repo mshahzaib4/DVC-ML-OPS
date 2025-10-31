902afb7f565275774681fabdeed1d9726da94008
shahzaib04 <shahzaibmalik0448@gmail.com>
2025-10-31 12:17:45 -0700

    Second version of data save

--- code.py
+++ code.py
@@ -3,8 +3,17 @@ import os
 
 data = {
     "Name": ["Shahzaib", "Zohaib", "Abdullah"],
-    "age":  ["25", "15", "22"],
-    "citty":["Narowal", "Narowal", "Narowal"]
+    "Age":  ["25", "15", "22"],
+    "City":["Narowal", "Narowal", "Narowal"]
 }
 df = pd.DataFrame(data)
-df.to_csv(df, "data_sample")
+
+new_row = {"Name": "Ali", "Age":"31", "City":"Lahore"}
+df.loc[len(df.index)] = new_row
+
+os.makedirs("data", exist_ok=True)
+
+file_path = os.path.join("data", "sample.csv")
+
+df.to_csv(file_path, index=False)
+print(f"File are save to {file_path}")
