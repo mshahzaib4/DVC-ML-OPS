015eb2b7661d82cf5ab6706d08d6adf515deebf0
shahzaib04 <shahzaibmalik0448@gmail.com>
2025-10-31 11:40:53 -0700

    Init DVC-ML-OPS

---
+++ code.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import os
+
+data = {
+    "Name": ["Shahzaib", "Zohaib", "Abdullah"],
+    "age":  ["25", "15", "22"],
+    "citty":["Narowal", "Narowal", "Narowal"]
+}
+df = pd.DataFrame(data)
+df
